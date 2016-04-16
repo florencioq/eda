00250fe70bacf0185de8fde26d3697fae66cbe94
Florencio Queiroz <florencioq@gmail.com>
2016-04-16 11:17:56 -0300

    Remoção de itens

--- Lista03/Q1.py
+++ Lista03/Q1.py
@@ -1,15 +1,14 @@
 from random import *
 
-
 class HashTable:
     def __init__(self, tamanho=20):
         self._tabela = tamanho * [None]
         self._n = 0
         self._primo = 109345121
         self._a = 1 + randrange(self._primo - 1)
-        self._a = 87924234
+        self._a = 787687
         self._b = randrange(self._primo)
-        self._b = 44287322
+        self._b = 554755
         self.colisoes = 0
 
     class _Elemento:
@@ -27,26 +26,57 @@ class HashTable:
     def _printHash(self):
         for i in range(len(self._tabela)):
             if self._tabela[i] == None:
-                print(0)
+                print("None")
             else:
                 print(str(self._tabela[i]._chave) + " "+ str( self._tabela[i]._valor))
 
-    def getitem(self, k):
-        j = self._funcao_hash(k)
-        if self._tabela[j]._chave == k:
-            return self._tabela[j]._valor
+    def remitem(self, k):
+        jj = self._funcao_hash(k)
+        j = jj
+        naoAchou = False
+        while self._tabela[j] == None or self._tabela[j]._chave != k:
+            j += 1
+            if j == len(self._tabela):
+                ## Chegou ao final
+                j = 0
+                while self._tabela[j] == None or self._tabela[j]._chave != k:
+                    j += 1
+                    if j == jj or j == len(self._tabela):
+                        naoAchou = True
+                        break
+            if naoAchou:
+                break
+        if naoAchou:
+            return False
         else:
-            while self._tabela[j]._chave != k:
-                j += 1
-                if j == self.tamanho():
-                    break
-            if j == self.tamanho():
-                return None
-            else:
-                return self._tabela[j]._valor
+            self._tabela[j] = None
+            return True
+
+    def getitem(self, k):
+        jj = self._funcao_hash(k)
+        j = jj
+        naoAchou = False
+        while self._tabela[j] == None or self._tabela[j]._chave != k:
+            j += 1
+            if j == len(self._tabela):
+                ## Chegou ao final
+                j = 0
+                while self._tabela[j] == None or self._tabela[j]._chave != k:
+                    j += 1
+                    if j == jj or j == len(self._tabela):
+                        naoAchou = True
+                        break
+            if naoAchou:
+                break
+        if naoAchou:
+            return None
+        else:
+            return self._tabela[j]._valor
+
 
     def setitem(self, k, num):
-        j = self._funcao_hash(k)
+        jj = self._funcao_hash(k)
+        j = jj
         elemento = self._Elemento(k, num)
 
         ## Buscando o elemento
@@ -59,7 +89,16 @@ class HashTable:
             while j < self.tamanho() and self._tabela[j] != None:
                 j += 1
             if j == self.tamanho():
-                hashCheio = True
+                # Circulando
+                j = 0
+                while j < jj and self._tabela[j] != None:
+                    j += 1
+                if j == jj:
+                    hashCheio = True
+                else:
+                    self._tabela[j] = elemento
+                    self._n += 1
+                    hashCheio = False
             else:
                 self._tabela[j] = elemento
                 self._n += 1
@@ -72,19 +111,28 @@ class HashTable:
 
 if __name__ == '__main__':
 
-    n = int(input("Entre com o tamanho do vetor: "))
+    n = int(input("Entre com o número de chaves: "))
+
+    # O bucket terá a metade do numero de chaves
     myHashTable = HashTable(int(n/2))
 
-    print(myHashTable.tamanho())
-    print(myHashTable.colisoes)
-    print("=================")
+    print("Incluindo alguns elementos")
+    print("=========================")
     for i in range(1,n+1):
         if not myHashTable.setitem(i, randint(1, 1000)):
             print("Colisão")
-        print("------------")
+        print("-------------------------")
         myHashTable._printHash()
-    print("=================")
+    print("=========================")
+    print("Buscando alguns elementos")
+    print("=========================")
     for i in range(1,n+1):
         retorno = myHashTable.getitem(i)
         if retorno != None:
             print(str(i)+"-"+str(retorno))
+    print("=========================")
+    print("Eliminando alguns elementos")
+    print("=========================")
+    for i in range (1,n,2):
+        retorno = myHashTable.remitem(i)
+    myHashTable._printHash()
